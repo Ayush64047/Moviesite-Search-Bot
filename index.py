830394d56f9ba97b6cb197a5a8f9da5478bdc6a3
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-04-19 16:39:46 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://moviesite-search-bot-w4mx.vercel.app"
+URL = "https://moviesite-search-bot-w4mx-git-main-ayush64047.vercel.app"
 bot = Bot(TOKEN)
 
 def welcome(update, context) -> None:
