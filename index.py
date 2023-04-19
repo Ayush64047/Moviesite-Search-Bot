bcc218e5062027e0fe462e9377fc53646e465502
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-04-19 14:42:31 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = ""
+URL = "https://moviesite-search-bot-gray.vercel.app"
 bot = Bot(TOKEN)
 
 def welcome(update, context) -> None:
